2a70dfe57c3f286e785b64822a8eaafaa90014c0
osemenso7 <oscarsemente100@gmail.com>
2021-02-28 19:41:38 +0100

    Tests de Insult, Comeback, Shout y Error.

--- ComUtils/src/test/java/ComUtilsTest.java
+++ ComUtils/src/test/java/ComUtilsTest.java
@@ -82,4 +82,72 @@ public class ComUtilsTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void insult_test() {
+        File file = new File("test");
+        try {
+            file.createNewFile();
+            Communication com = new Communication(new FileInputStream(file), new FileOutputStream(file));
+            com.write_insult("Obtuve esta cicatriz en una batalla a muerte!");
+            String readedStr = com.reader();
+
+            assertEquals("Obtuve esta cicatriz en una batalla a muerte!", readedStr);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void comeback_test() {
+        File file = new File("test");
+        try {
+            file.createNewFile();
+            Communication com = new Communication(new FileInputStream(file), new FileOutputStream(file));
+            com.write_comeback("Espero que ya hayas aprendido a no tocarte la nariz");
+            String readedStr = com.reader();
+
+            assertEquals("Espero que ya hayas aprendido a no tocarte la nariz", readedStr);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shout_test() {
+        File file = new File("test");
+        try {
+            file.createNewFile();
+            Communication com = new Communication(new FileInputStream(file), new FileOutputStream(file));
+            com.write_shout("!He ganado, Name2!");
+            String readedStr = com.reader();
+
+            assertEquals("!He ganado, Name2!", readedStr);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void error_test() {
+        File file = new File("test");
+        try {
+            file.createNewFile();
+            Communication com = new Communication(new FileInputStream(file), new FileOutputStream(file));
+            com.write_insult("Espero que ya hayas aprendido a no tocarte la nariz!");
+            String readedStr = com.reader();
+
+             if (!readedStr.equals("Espero que ya hayas aprendido a no tocarte la nariz")){
+                 com.write_error("!Mensaje incompleto, grumete! !Hasta la vista!");
+                 readedStr = com.reader();
+                 assertEquals("!Mensaje incompleto, grumete! !Hasta la vista!", readedStr);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
